36186e2fa548ff7f7ec13016b33fd1139ef76e66
Eugene Sokolov <eugsokolov@gmail.com>
2015-02-07 19:21:20 -0500

    Update yylval.h

--- yylval.h
+++ yylval.h
@@ -1,7 +1,29 @@
-typedef union{
 
-  char *string_literal;
-  int integer;
+
+#ifndef _YYLVAL_H
+#define _YYLVAL_H
+
+enum number_type{
+  SIZE_INT=0,
+  SIZE_LONG,
+  SIZE_LONGLONG,
+  SIZE_FLOAT,
+  SIZE_DOUBLE,
+  SIZE_LONGDOUBLE,
+  SIZE_UNSIGNED,
+  SIZE_SIGNED
+};
+
+typedef union{
+  char yychar;
+  char *yystring;
+  //int yyint;
+  unsigned long long int yyint;
+  long double yydouble;
+  enum number_type num_type;
+  enum number_type num_sign;
 } YYSTYPE;
 
 extern YYSTYPE yylval;
+
+#endif
