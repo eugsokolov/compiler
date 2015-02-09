c50d81130b1747fdfc4b1ec206e1bee8d4551e2b
Eugene Sokolov <eugsokolov@gmail.com>
2015-02-08 20:26:08 -0500

    Update yylval.h

--- yylval.h
+++ yylval.h
@@ -8,26 +8,25 @@ LEXICAL ANALYSIS: YYLVAL definition
 #define _YYLVAL_H
 
 enum number_type{
-  TYPE_INT=0,
-  TYPE_LONG,
-  TYPE_LONGLONG,
-  SIZE_FLOAT,
-  TYPE_DOUBLE,
-  TYPE_LONGDOUBLE,
-  TYPE_UNSIGNED,
-  TYPE_SIGNED
+	TYPE_INT=0,
+	TYPE_LONG,
+	TYPE_LONGLONG,
+	TYPE_FLOAT,
+	TYPE_DOUBLE,
+	TYPE_LONGDOUBLE,
+	TYPE_UNSIGNED,
+	TYPE_SIGNED
 };
 
 typedef union{
-
-  char yychar;
-  char *yystring;
-  unsigned int yystring_size;
-  //int yyint;
-  unsigned long long int yyint;
-  long double yydouble;
-  enum number_type num_type;
-  enum number_type num_sign;
+	char yychar;
+	char *yystring;
+	unsigned int yystring_size;
+	//int yyint;
+	unsigned long long int yyint;
+	long double yydouble;
+	enum number_type num_type;
+	enum number_type num_sign;
 } YYSTYPE;
 
 #define MAX_STRING_LENGTH 4096
